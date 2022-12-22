16a96a0767940aed84b844d56848647ee2af800b
lvzhenchao <1370164516@qq.com>
2022-12-22 10:31:13 +0800

    Update 14-异步IO-协程.py

--- 14-异步IO-协程.py
+++ 14-异步IO-协程.py
@@ -1,4 +1,6 @@
 
+# python对协程的支持是通过generator生成器实现的
+
 # 协程优势
 ## 1、极高的执行效率，因为子程序切换不是线程切换，而是程序自身控制，没有线程切换的开销
 ## 2、不需要多线程的锁机制，因为只有一个线程，也不存在同时写变量冲突，在协程中控制共享资源不加锁，只需要判断状态就好了
@@ -35,6 +37,42 @@ o = odd()
 next(o)
 next(o)
 next(o)
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    c.send(None)# 1、 首先调用c.send(None)启动生成器
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+c = consumer()
+produce(c)
+
+#注意到consumer函数是一个generator，把一个consumer传入produce后：
+
+#首先调用c.send(None)启动生成器；
+
+#然后，一旦生产了东西，通过c.send(n)切换到consumer执行；
+
+#consumer通过yield拿到消息，处理，又通过yield把结果传回；
+
+#produce拿到consumer处理的结果，继续生产下一条消息；
+
+#produce决定不生产了，通过c.close()关闭consumer，整个过程结束。
+
+#整个流程无锁，由一个线程执行，produce和consumer协作完成任务，所以称为“协程”，而非线程的抢占式多任务。
 
 
 
